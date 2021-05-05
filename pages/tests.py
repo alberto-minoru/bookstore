974037f9b07c45654af9c12e6f734bf6fd813148
Alberto Minoru <alberto.minoru@gmail.com>
2021-05-05 19:47:42 -0300

    Tests with setUp and HomePageView test

--- pages/tests.py
+++ pages/tests.py
@@ -1,25 +1,26 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 
 class HomepageTests(SimpleTestCase):
 
-    def test_homepage_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self) -> None:
+        url = reverse('home')
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
 
     def test_homepage_contains_correct_html(self):
-        response = self.client.get('/')
-        self.assertContains(response, 'Homepage')
+        self.assertContains(self.response, 'Homepage')
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get('/')
-        self.assertNotContains(response, 'Hi there! I should not be on the page.')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_homepage_url_resolves_homepage_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
