ba4f33425bb2999fd59d2be9c40c02d107d19576
Alberto Minoru <alberto.minoru@gmail.com>
2021-05-10 17:09:33 -0300

    Crispy Forms

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party
+    'crispy_forms',
+
+    # Local
     'accounts',
     'pages',
 ]
@@ -132,3 +136,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
