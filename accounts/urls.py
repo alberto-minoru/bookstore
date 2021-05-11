0d9438249c44652d72303b1896aac4d3e170894d
Alberto Minoru <alberto.minoru@gmail.com>
2021-05-11 19:27:34 -0300

    Change to django-allauth account management

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import SignupPageView
-
-urlpatterns = [
-    path('signup/', SignupPageView.as_view(), name='signup'),
-]
+# Não mais necessário apos a adição do allauth
+# from django.urls import path
+# from .views import SignupPageView
+#
+# urlpatterns = [
+#     path('signup/', SignupPageView.as_view(), name='signup'),
+# ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from django.urls import reverse_lazy
-from django.views import generic
-from .forms import CustomUserCreationForm
-
-
-class SignupPageView(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+# Não mais necessário apos a adição do allauth
+# from django.urls import reverse_lazy
+# from django.views import generic
+# from .forms import CustomUserCreationForm
+#
+#
+# class SignupPageView(generic.CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'registration/signup.html'
