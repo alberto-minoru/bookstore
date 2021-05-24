c80845bbd2563ded4bdc177eaf4f6996b9dd113b
Alberto Minoru <alberto.minoru@gmail.com>
2021-05-24 19:38:36 -0300

    Media and image upload conf

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
